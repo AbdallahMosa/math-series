65f563bfa73dcfd3052ffba0a92efb2222a4850b
AbdallahMosa <abdallahmosa95@yahoo.com>
2022-10-06 12:58:46 +0300

    fix

--- math_series/series.py
+++ math_series/series.py
@@ -1,4 +1,7 @@
 def fibonacci(n):
+    """
+       fibonacci series where each number is the sum of the two that precede it. It starts from 0 and 1 usually. The  sequence is given by 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, and so on
+    """
     if type(n)!=int:
        return "Sorry,You entered a String"
     if n < 0:
@@ -11,6 +14,12 @@ def fibonacci(n):
         return(fibonacci(n-1) + fibonacci(n-2))
 
 def lucas(n):
+
+    """
+    lucas series where each number is the sum of the two that precede it. It starts from 2 and 1 usually
+     ex : 2, 1, 3, 4, 7, 11, 18, 29, 47, 76, 123
+
+    """
     if type(n)!=int:
        return "Sorry,You entered a String"
     if n < 0:
@@ -24,6 +33,11 @@ def lucas(n):
 
 # sum_series
 def sum_series(n,x=0 ,y=1):
+    """
+     this function sum_series where each number is the sum of the two that precede it. if  starts from 0 and 1 can we call it, if  starts from 2 and 1 can we call it lucas  Otherwise you can starts with any number you want 
+    
+
+    """
     if type(n)!=int or type(x)!=int  or type(y)!=int:
        return "Sorry,You entered a String"
     if n < 0 or x < 0 or y< 0:
